eeafce37d33b9e414d677824d3c016344d941b0a
root <tshepangkeabetswe44@gmail.com>
2024-01-02 16:42:49 -0800

    alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
-for char in range(ord('a'), ord('z')+1):
-    if chr(char) not in ['q', 'e']:
-        print(f"{chr(char)}", end='')
+#!/usr/bin/python3
+for alpha in range(ord("a"), ord("z") + 1):
+    if alpha != ord("q") and alpha != ord("e"):
+        print("{}".format(chr(alpha)), end='')
