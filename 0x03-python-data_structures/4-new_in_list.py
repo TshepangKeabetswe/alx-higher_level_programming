d41886f406eb7b575aa83759e9b90f667748d50e
root <tshepangkeabetswe44@gmail.com>
2024-01-09 13:07:18 -0800

    new in list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if my_list is None:
+        return None
+    if idx < 0 or idx >= len(my_list):
+        return my_list.copy()
+    else:
+        my_new_list = my_list.copy()
+        my_new_list[idx] = element
+        return my_new_list
